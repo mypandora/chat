2c3a0b58f02257fb0c7244a4515714f9404839c0
BrendanEichDisciple <BrendanEichDisciple@hotmail.com>
2024-06-25 20:31:14 +0800

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,1 @@
+export function Chat(element: HTMLButtonElement): void;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./main.js", "./lib/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
